69bdd1313e21797819627279dbaf9e8ecc527d1c
mteiska <mika123450@hotmail.com>
2022-11-13 23:24:07 +0200

    Adding assignment 2 and 3

--- node_server.js
+++ node_server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 const app = express();
 app.use(express.json());
+var bodyParser = require('body-parser') 
 // Help from https://expressjs.com/en/starter/hello-world.html 
+var jsonParser = bodyParser.json()
 app.listen(3000, () => {
   })
 
@@ -9,4 +11,29 @@ app.get('/hello', (req, res) => {
     res.send({
         msg: "Hello world"
     })
+  })
+  //Help from: https://www.digitalocean.com/community/tutorials/nodejs-req-object-in-expressjs
+  app.get('/echo/:id', (req, res) => {
+    res.send({
+         id: req.params.id
+    })
+  })
+  app.post('/sum',jsonParser, (req, res) => {
+    let count = 0
+    for ( const number of req.body.numbers){
+
+            count = count + Number(number)
+           
+        
+        }
+        res.send({
+            sum: count
+       })
+         
+        
+        
+    
+    
+ 
+
   })
